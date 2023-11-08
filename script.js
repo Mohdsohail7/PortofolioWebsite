0ab19ee8c88b7c56a3838d14f02a72a83aa395a2
Mohdsohail7 <sohailmohd437@gmail.com>
2023-11-08 23:41:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_container = document.querySelector(".navbar");
+
+
+const toggleNavbar = () => {
+    nav_container.classList.toggle("active");
+}
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
